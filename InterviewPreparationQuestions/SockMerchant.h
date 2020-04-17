ea19275e2fc672cfb923f880c845e175958ffb91
Cavan Biggs <cbiggs7635@conestogac.on.ca>
2020-04-17 13:44:47 -0400

    Initial setup complete

---
+++ InterviewPreparationQuestions/SockMerchant.h
@@ -0,0 +1,6 @@
+#pragma once
+//Challenge URL: https://www.hackerrank.com/challenges/sock-merchant/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+
+#include "common.h"
+void SolutionRunner();
+int sockMerchant(int n, vector<int> ar);
--- InterviewPreparationQuestions/common.h
+++ InterviewPreparationQuestions/common.h
@@ -1,1 +1,12 @@
 #pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <istream>
+#include <ostream>
+#include <fstream>
+#include <iterator>
+#include <algorithm>
+
+
+using namespace std;
